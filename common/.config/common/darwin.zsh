c92f0e35e875b64768f288ff83a458f330937443
Huy Nguyen <giahuy2201@icloud.com>
2024-03-30 13:35:50 -0400

    Enable program configs only when installed (macOS)

--- common/.config/common/darwin.zsh
+++ common/.config/common/darwin.zsh
@@ -1,10 +1,21 @@
+# user local bins
+if [ -d $HOME/.local/bin ]; then
+  export PATH="$HOME/.local/bin:$PATH"
+fi
+
 # HomeBrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [ -f /opt/homebrew/bin/brew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 # Poetry
-export PATH="$HOME/.local/bin:$PATH"
-export POETRY_CONFIG_DIR="$HOME/.config/pypoetry"
+if [ -f $HOME/.local/bin/poetry ]; then
+  export POETRY_CONFIG_DIR="$HOME/.config/pypoetry"
+fi
+
 # Node Version Manager
-export NVM_DIR="$HOME/.nvm"
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"                                       # This loads nvm
-[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+if [ -d $HOME/.nvm ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"                                       # This loads nvm
+  [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+fi
