b4d22d768033091f273eace6c695aa584b414f3d
Huy Nguyen <giahuy2201@icloud.com>
2024-05-04 12:04:16 -0400

    Add script to tweak gnome

---
+++ scripts/gnome.sh
@@ -0,0 +1,13 @@
+# Set fonts
+gsettings set org.gnome.desktop.interface font-name 'Hack Nerd Font 13'
+gsettings set org.gnome.desktop.interface document-font-name 'Hack Nerd Font 13'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Hack Nerd Font 13'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Hack Nerd Font 13'
+# Set theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+# Set top bar
+gsettings set org.gnome.desktop.interface clock-show-weekday 'true'
+# Set title bar
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
+# Set keyboard
+gsettings set org.gnome.desktop.input-sources xkb-options "['terminate:ctrl_alt_bksp', 'ctrl:swap_lalt_lctl']"
