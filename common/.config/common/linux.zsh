5c943832b9bb513f1e5b2c3f4f4045f7d33bb1d2
Huy Nguyen <giahuy2201@icloud.com>
2024-05-11 13:54:55 -0400

    Add nvm to linux zsh config

--- common/.config/common/linux.zsh
+++ common/.config/common/linux.zsh
@@ -12,3 +12,10 @@ fi
 if [ -d $HOME/.acme.sh ]; then
   . "$HOME/.acme.sh/acme.sh.env"
 fi
+
+# Node Version Manager
+if [ -d $HOME/.nvm ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
