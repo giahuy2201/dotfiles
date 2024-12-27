4d97aabf8648ff1828c1a1f4dff810ba1fed5f54
Huy Nguyen <giahuy2201@icloud.com>
2024-12-27 14:33:25 -0500

    Add PyEnv and Poetry to mac and linux zsh common config

--- common/.config/common/darwin.zsh
+++ common/.config/common/darwin.zsh
@@ -13,6 +13,12 @@ if [ -f $HOME/.local/bin/poetry ]; then
   export POETRY_CONFIG_DIR="$HOME/.config/pypoetry"
 fi
 
+# Pyenv
+if [ -f $HOME/.pyenv/bin/pyenv ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+
 # Node Version Manager
 if [ -d $HOME/.nvm ]; then
   export NVM_DIR="$HOME/.nvm"
--- common/.config/common/linux.zsh
+++ common/.config/common/linux.zsh
@@ -13,6 +13,17 @@ if [ -d $HOME/.acme.sh ]; then
   . "$HOME/.acme.sh/acme.sh.env"
 fi
 
+# Poetry
+if [ -f $HOME/.local/bin/poetry ]; then
+  export POETRY_CONFIG_DIR="$HOME/.config/pypoetry"
+fi
+
+# Pyenv
+if [ -f $HOME/.pyenv/bin/pyenv ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+fi
+
 # Node Version Manager
 if [ -d $HOME/.nvm ]; then
   export NVM_DIR="$HOME/.nvm"
