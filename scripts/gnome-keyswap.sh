375185452b64855882477f07c8ad90f36d2bb7f9
Huy Nguyen <giahuy2201@icloud.com>
2025-01-09 23:01:25 -0500

    Move gnome keyswaps to seperate script

---
+++ scripts/gnome-keyswap.sh
@@ -0,0 +1,2 @@
+
+gsettings set org.gnome.desktop.input-sources xkb-options "['terminate:ctrl_alt_bksp', 'ctrl:swap_lalt_lctl']"
--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -3,8 +3,6 @@
 gsettings set org.gnome.desktop.interface clock-show-weekday 'true'
 # Set title bar
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,close'
-# Set keyboard
-gsettings set org.gnome.desktop.input-sources xkb-options "['terminate:ctrl_alt_bksp', 'ctrl:swap_lalt_lctl']"
 # Set windows switching
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "[]"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab', '<Super>Tab']"
