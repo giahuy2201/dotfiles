156c088f45c71a24679d5f9e483803ea65c30397
Huy Nguyen <giahuy2201@icloud.com>
2025-02-05 23:57:41 -0500

    Add option meanings to gnome keyboard script

--- scripts/gnome-keyswap.sh
+++ scripts/gnome-keyswap.sh
@@ -1,2 +1,4 @@
-
+# Options:
+# 'terminate:ctrl_alt_bksp' : Key sequence to kill the X server
+# 'ctrl:swap_lalt_lctl' : Swap Left Alt with Left Ctrl
 gsettings set org.gnome.desktop.input-sources xkb-options "['terminate:ctrl_alt_bksp', 'ctrl:swap_lalt_lctl']"
