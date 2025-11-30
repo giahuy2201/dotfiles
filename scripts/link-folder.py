e25c131083e57c0e15478bcc478462889f243408
Huy Nguyen <giahuy2201@icloud.com>
2025-11-30 01:05:03 -0500

    Migrate link_show.py from scripts repo over

---
+++ scripts/link-folder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+# command line arguments
+argv = sys.argv
+if len(argv) < 2 or argv[1] == '-h':
+    print("""\
+Hard link all files from source folder to destination folder
+
+Usage: python {} src_dir dst_dir
+Options:
+-r  Apply to subsequent folders\
+""".format(argv[0]))
+    sys.exit(1)
+
+def rlink(src,dst,recursive=True):
+    for f in os.scandir(src):
+        if f.is_file() and not f.is_symlink():
+            dst_f = os.path.join(dst,f.name)
+            os.link(f,dst_f)
+            print(dst_f)
+        elif f.is_dir() and recursive:
+            dst_sub = dst+'/'+f.name
+            os.mkdir(dst_sub)
+            rlink(src+'/'+f.name,dst_sub,True)
+
+if argv[1] == '-r':
+    src = argv[2]
+    dst = argv[3]
+else:
+    src = argv[1]
+    dst = argv[2]
+
+try:
+    os.mkdir(dst)
+except FileExistsError:
+    pass
+except FileNotFoundError:
+    print('Error: Parent directory not found!',dst)
+
+rlink(src,dst)
